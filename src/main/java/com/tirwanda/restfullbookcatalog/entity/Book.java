837e27658656c36af01928fce042b968f5786e36
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-06-26 14:55:03 +0700

    Create Entity Book

---
+++ src/main/java/com/tirwanda/restfullbookcatalog/entity/Book.java
@@ -0,0 +1,59 @@
+package com.tirwanda.restfullbookcatalog.entity;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table()
+public class Book implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3569117756925183123L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column()
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
